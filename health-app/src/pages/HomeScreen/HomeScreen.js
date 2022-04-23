ec630f7889584a7dae7043c274b4e10e56d94bbb
RNostrand <RNostrand18@yahoo.com>
2022-04-22 19:00:49 -0700

    Added basic CSS for the home page

--- health-app/src/pages/HomeScreen/HomeScreen.js
+++ health-app/src/pages/HomeScreen/HomeScreen.js
@@ -1,18 +1,51 @@
-import React from 'react'
-import { Text, View, TouchableOpacity } from 'react-native';
-import {db, authentication, handleSignOut} from "../../firebase/config";
-import { createUserWithEmailAndPassword,signInWithEmailAndPassword, getAuth} from "firebase/auth";
-import { collection, getDoc,updateDoc,onSnapshot,deleteDoc,doc,setDoc, waitForPendingWrites} from "firebase/firestore";
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { handleSignOut } from "../../firebase/config";
+import styles from './styles';
+
 
 export default function HomeScreen(props) {
     console.log(props.user.email);
     return (
-        <View>
-            <Text>Home Screen</Text>
-            <TouchableOpacity
-                    onPress={() => handleSignOut()}>
-                    <Text>Sign Out</Text>
+        <View style={styles.container}>
+            <KeyboardAwareScrollView
+                style={{ flex: 1, width: '100%' }}
+                keyboardShouldPersistTaps="always">
+
+                <View
+                style={{ flex: 1, width: '100%', flexDirection: "row" }}>
+                    <Text style={styles.title}>Welcome, USER!</Text>
+
+                    <TouchableOpacity
+                        style={styles.profile}
+                        onPress={() => onLoginPress()}>
+                        <Text style={styles.buttonTitle}>View Profile</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <Text style={styles.info}>
+                    Some stuff about the app and the services we offer.
+                    Stuff about how to navigate the app.
+                </Text>
+                <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => onLoginPress()}>
+                        <Text style={styles.buttonTitle}>Update Progress</Text>
                 </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => onLoginPress()}>
+                    <Text style={styles.buttonTitle}>View Progress</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => handleSignOut()}>
+                    <Text style={styles.buttonTitle}>Manage Goals</Text>
+                </TouchableOpacity>
+            </KeyboardAwareScrollView>
         </View>
     )
 }
--- health-app/src/pages/HomeScreen/styles.js
+++ health-app/src/pages/HomeScreen/styles.js
@@ -1,19 +1,51 @@
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { handleSignOut } from "../../firebase/config";
+import styles from './styles';
 
-export default StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center'
-    },
-    input: {
-        height: 48,
-        borderRadius: 5,
-        overflow: 'hidden',
-        backgroundColor: 'white',
-        marginTop: 10,
-        marginBottom: 10,
-        marginLeft: 30,
-        marginRight: 30,
-        paddingLeft: 16
-    }
-})
+
+export default function HomeScreen(props) {
+    console.log(props.user.email);
+    return (
+        <View style={styles.container}>
+            <KeyboardAwareScrollView
+                style={{ flex: 1, width: '100%' }}
+                keyboardShouldPersistTaps="always">
+
+                <View
+                style={{ flex: 1, width: '100%', flexDirection: "row" }}>
+                    <Text style={styles.title}>Welcome, USER!</Text>
+
+                    <TouchableOpacity
+                        style={styles.profile}
+                        onPress={() => onLoginPress()}>
+                        <Text style={styles.buttonTitle}>View Profile</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <Text style={styles.info}>
+                    Some stuff about the app and the services we offer.
+                    Stuff about how to navigate the app.
+                </Text>
+                <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => onLoginPress()}>
+                        <Text style={styles.buttonTitle}>Update Progress</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => onLoginPress()}>
+                    <Text style={styles.buttonTitle}>View Progress</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => handleSignOut()}>
+                    <Text style={styles.buttonTitle}>Manage Goals</Text>
+                </TouchableOpacity>
+            </KeyboardAwareScrollView>
+        </View>
+    )
+}

f0f944d84af94cb5a699e10a4f4232cfbe40949c
Jatnael Montes <ifelsejet@gmail.com>
2022-04-25 01:48:45 -0700

    Update updateProgress.js
    Added alert + CSS

--- health-app/src/pages/updateProgress/updateProgress.js
+++ health-app/src/pages/updateProgress/updateProgress.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { ScrollView, Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { ScrollView, Image, Text, TextInput, TouchableOpacity, View, Alert } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import {useAuth, authentication} from "../../firebase/config";
 import { createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
@@ -10,11 +10,22 @@ import { AntDesign } from "@expo/vector-icons";
 import styles from './styles';
 
 
+
 export default function UpdateProgress(props) {
+    const showAlert = () =>  
+    Alert.alert(  
+        'Update Progress',  
+        'Progress was successfuly updated!',  
+        [  
+            
+            {text: 'OK', onPress: () => props.navigation.navigate('Home')},  
+        ]  
+    );  
+
+    let [name, setName] = useState("");
     let [weight, setWeight] = useState("");
     let [age, setAge] = useState("");
-    let [height, setHeight] = useState("");
-    let [fitness, setFitness] = useState("");
+
     console.log("Built differently");
     console.log(props.user.uid);
        
@@ -28,8 +39,50 @@ export default function UpdateProgress(props) {
         <Text style={styles._heading}>Update Progress</Text>
         <Text style={styles._subheading}>Update any or all of the following progress for today.</Text>
         <ScrollView showsVerticalScrollIndicator={false}>
-        <Text style={styles._title}>Push Ups:</Text>
-    
+        <Text style={styles._title}>Push Ups!:</Text>
+        <View style={styles._input_main}>
+            <TextInput
+                style={styles._textinput}
+                placeholder=""
+                placeholderTextColor="#000"
+                value={name}
+                onChangeText={name => setName(name)}
+                secureTextEntry={false}
+            />
+        </View>
+        <Text style={styles._title}>Jumping {"\n"} Jacks:</Text>
+        <View style={styles._input_main}>
+            <TextInput
+                style={styles._textinput}
+                placeholder=""
+                placeholderTextColor="#000"
+                value={weight}
+                onChangeText={weight => setWeight(weight)}
+                secureTextEntry={false}
+            />
+        </View>
+        <Text style={styles._title}>Squats</Text>
+        <View style={styles._input_main}>
+            <TextInput
+                style={styles._textinput}
+                placeholder=""
+                placeholderTextColor="#000"
+                value={age}
+                onChangeText={age => setAge(age)}
+                secureTextEntry={false}
+            />
+            </View>
+         <View style={styles._leftButton}>
+            <TouchableOpacity style={styles._btn} onPress={() => console.log(props.user.uid)}>
+            <Text style={styles._btn_text}> Add a New Goal </Text>
+            </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles._btn} onPress={() =>  showAlert()}>
+            <Text style={styles._btn_text}> Update Progress </Text>
+        </TouchableOpacity>
+        <View style={{ paddingBottom: 20 }} />
+
+        
         </ScrollView>
 
 

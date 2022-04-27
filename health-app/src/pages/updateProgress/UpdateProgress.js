9c29ac23fef97fdf62c46651d89661bf507f7d92
samishsK <samish.sk@gmail.com>
2022-04-26 20:36:57 -0700

    Update Progress page styles fixed

--- health-app/src/pages/updateProgress/UpdateProgress.js
+++ health-app/src/pages/updateProgress/UpdateProgress.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import { ScrollView, Image, Text, TextInput, TouchableOpacity, View, Alert } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import {useAuth, authentication} from "../../firebase/config";
-import { createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+import { useAuth, authentication } from "../../firebase/config";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { AntDesign } from "@expo/vector-icons";
 
 
@@ -12,15 +12,15 @@ import styles from './styles';
 
 
 export default function UpdateProgress(props) {
-    const showAlert = () =>  
-    Alert.alert(  
-        'Update Progress',  
-        'Progress was successfuly updated!',  
-        [  
-            
-            {text: 'OK', onPress: () => props.navigation.navigate('View')},  
-        ]  
-    );  
+    const showAlert = () =>
+        Alert.alert(
+            'Update Progress',
+            'Progress was successfuly updated!',
+            [
+
+                { text: 'OK', onPress: () => props.navigation.navigate('View') },
+            ]
+        );
 
     let [name, setName] = useState("");
     let [weight, setWeight] = useState("");
@@ -28,63 +28,66 @@ export default function UpdateProgress(props) {
 
     console.log("Built differently");
     console.log(props.user.uid);
-       
+
 
     return (
         <View style={styles.container}>
-        <TouchableOpacity style={styles._back} onPress={() => props.navigation.goBack()}>
-      <AntDesign name="back" size={24} color="black" />
-        </TouchableOpacity>
+            <TouchableOpacity style={styles._back} onPress={() => props.navigation.goBack()}>
+                <AntDesign name="back" size={24} color="black" />
+            </TouchableOpacity>
 
-        <Text style={styles._heading}>Update Progress</Text>
-        <Text style={styles._subheading}>Update any or all of the following progress for today.</Text>
-        <ScrollView showsVerticalScrollIndicator={false}>
-        <Text style={styles._title}>Push Ups!:</Text>
-        <View style={styles._input_main}>
-            <TextInput
-                style={styles._textinput}
-                placeholder=""
-                placeholderTextColor="#000"
-                value={name}
-                onChangeText={name => setName(name)}
-                secureTextEntry={false}
-            />
+            <Text style={styles._heading}>Update Progress</Text>
+            <Text style={{
+                marginLeft: 10, fontFamily: 'Comfortaa-Regular',
+                fontSize: 13,
+            }}>Update any or all of the following progress for today.</Text>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Text style={styles._title}>Push Ups!:</Text>
+                <View style={styles._input_main}>
+                    <TextInput
+                        style={styles._textinput}
+                        placeholder=""
+                        placeholderTextColor="#000"
+                        value={name}
+                        onChangeText={name => setName(name)}
+                        secureTextEntry={false}
+                    />
+                </View>
+                <Text style={styles._title}>Jumping {"\n"} Jacks:</Text>
+                <View style={styles._input_main}>
+                    <TextInput
+                        style={styles._textinput}
+                        placeholder=""
+                        placeholderTextColor="#000"
+                        value={weight}
+                        onChangeText={weight => setWeight(weight)}
+                        secureTextEntry={false}
+                    />
+                </View>
+                <Text style={styles._title}>Squats</Text>
+                <View style={styles._input_main}>
+                    <TextInput
+                        style={styles._textinput}
+                        placeholder=""
+                        placeholderTextColor="#000"
+                        value={age}
+                        onChangeText={age => setAge(age)}
+                        secureTextEntry={false}
+                    />
+                </View>
+
+                <TouchableOpacity style={styles._btn} onPress={() => showAlert()}>
+                    <Text style={styles._btn_text}> Update and View Progress </Text>
+                </TouchableOpacity>
+                <View style={{ paddingBottom: 20 }} />
+
+
+            </ScrollView>
+
+
+
+
         </View>
-        <Text style={styles._title}>Jumping {"\n"} Jacks:</Text>
-        <View style={styles._input_main}>
-            <TextInput
-                style={styles._textinput}
-                placeholder=""
-                placeholderTextColor="#000"
-                value={weight}
-                onChangeText={weight => setWeight(weight)}
-                secureTextEntry={false}
-            />
-        </View>
-        <Text style={styles._title}>Squats</Text>
-        <View style={styles._input_main}>
-            <TextInput
-                style={styles._textinput}
-                placeholder=""
-                placeholderTextColor="#000"
-                value={age}
-                onChangeText={age => setAge(age)}
-                secureTextEntry={false}
-            />
-            </View>
-        
-        <TouchableOpacity style={styles._btn} onPress={() =>  showAlert()}>
-            <Text style={styles._btn_text}> Update and View Progress </Text>
-        </TouchableOpacity>
-        <View style={{ paddingBottom: 20 }} />
+    );
 
-        
-        </ScrollView>
-
-
-       
-       
-    </View>
-  );
-    
 }

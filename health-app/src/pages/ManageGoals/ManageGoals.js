e713393c8f568cb7f973e401cf7d445c380c437c
samishsK <samish.sk@gmail.com>
2022-04-26 20:26:29 -0700

    Manage Goals page updated with goal amount

--- health-app/src/pages/ManageGoals/ManageGoals.js
+++ health-app/src/pages/ManageGoals/ManageGoals.js
@@ -1,26 +1,23 @@
 import React, { useState } from 'react'
 import { ScrollView, Modal, Image, Text, TextInput, TouchableOpacity, View, Alert } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import {useAuth, authentication} from "../../firebase/config";
-import { createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+import { useAuth, authentication } from "../../firebase/config";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { AntDesign } from "@expo/vector-icons";
-
-
-
 import styles from './styles';
 
 
 
 export default function ManageGoals(props) {
-    const showAlert = () =>  
-    Alert.alert(  
-        'Manage Goals',  
-        'Goals were successfuly updated!',  
-        [  
-            
-            {text: 'OK', onPress: () => props.navigation.navigate('Home')},  
-        ]  
-    );  
+    const showAlert = () =>
+        Alert.alert(
+            'Manage Goals',
+            'Goals were successfuly updated!',
+            [
+
+                { text: 'OK', onPress: () => props.navigation.navigate('Home') },
+            ]
+        );
 
     let [name, setName] = useState("");
     let [weight, setWeight] = useState("");
@@ -30,56 +27,37 @@ export default function ManageGoals(props) {
 
     console.log("Built differently!");
     console.log(props.user.uid);
-       
+
 
     return (
         <View style={styles.container}>
-        <TouchableOpacity style={styles._back} onPress={() => props.navigation.goBack()}>
-      <AntDesign name="back" size={24} color="black" />
-        </TouchableOpacity>
-
-        <Text style={styles._heading}>Manage Goals</Text>
-        <Text style={styles._subheading}>Below you can view your current daily goals. {"\n"}Update the daily goals as you wish.</Text>
-        <ScrollView showsVerticalScrollIndicator={false}>
-        <Text style={styles._title}>Push Ups!:</Text>
-        <View style={styles._input_main}>
-            <TextInput
-                style={styles._textinput}
-                placeholder=""
-                placeholderTextColor="#000"
-                value={name}
-                onChangeText={name => setName(name)}
-                secureTextEntry={false}
-            />
-        </View>
-        <Text style={styles._title}>Jumping {"\n"} Jacks:</Text>
-        <View style={styles._input_main}>
-            <TextInput
-                style={styles._textinput}
-                placeholder=""
-                placeholderTextColor="#000"
-                value={weight}
-                onChangeText={weight => setWeight(weight)}
-                secureTextEntry={false}
-            />
-        </View>
-        <Text style={styles._title}>Squats</Text>
-        <View style={styles._input_main}>
-            <TextInput
-                style={styles._textinput}
-                placeholder=""
-                placeholderTextColor="#000"
-                value={age}
-                onChangeText={age => setAge(age)}
-                secureTextEntry={false}
-            />
-            </View>
-         <View style={styles._leftButton}>
-            <TouchableOpacity style={styles._btn} onPress={() => setModalVisible(!modalVisible)}>
-            <Text style={styles._btn_text}> Add a New Goal </Text>
+            <TouchableOpacity style={styles._back} onPress={() => props.navigation.goBack()}>
+                <AntDesign name="back" size={24} color="black" />
             </TouchableOpacity>
-        </View>
-        <View style={styles._centered_view}>
+
+            <Text style={styles._heading}>Manage Goals</Text>
+            <Text style={{
+                marginLeft: 10, fontFamily: 'Comfortaa-Regular',
+                fontSize: 13,
+            }}>Below you can view your current daily goals. {"\n"}Update the daily goals as you wish.</Text>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Text style={styles._title}>Push Ups:</Text>
+                <View style={styles._input_main}>
+                    <TextInput
+                        style={styles._textinput}
+                        placeholder=""
+                        placeholderTextColor="#000"
+                        value={name}
+                        onChangeText={name => setName(name)}
+                        secureTextEntry={false}
+                    />
+                </View>
+                <View style={styles._leftButton}>
+                    <TouchableOpacity style={styles._btn} onPress={() => setModalVisible(!modalVisible)}>
+                        <Text style={styles._btn_text}> New Goal </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles._centered_view}>
                     <Modal
                         animationType="slide"
                         transparent={true}
@@ -91,29 +69,35 @@ export default function ManageGoals(props) {
                     >
                         <View style={styles.modalView}>
                             <Text style={styles.modalText}>Goal Title:</Text>
-                            <View style={{borderColor:'black', borderWidth: 1}}>
-                            <TextInput
-                         placeholder=""
-                            />
+                            <Text style={styles.modalText1}>Goal Amount:</Text>
+                            <View style={{ borderColor: 'black', borderWidth: 1, height: 30, width: 150 }}>
+                                <TextInput
+                                    placeholder=""
+                                />
+                            </View>
+                            <View style={{ borderColor: 'black', borderWidth: 1, height: 30, width: 55, marginLeft: 175, marginTop: -30 }}>
+                                <TextInput
+                                    placeholder=""
+                                />
                             </View>
                             <TouchableOpacity style={styles._btn} onPress={() => setModalVisible(!modalVisible)}>
-                            <Text style={styles._btn_text}> Save New Goal! </Text>
+                                <Text style={styles._btn_text}> Save New Goal! </Text>
                             </TouchableOpacity>
                         </View>
                     </Modal>
                 </View>
-        <TouchableOpacity style={styles._btn} onPress={() =>  showAlert()}>
-            <Text style={styles._btn_text}> Update Goals </Text>
-        </TouchableOpacity>
-        <View style={{ paddingBottom: 20 }} />
-
-        
-        </ScrollView>
+                <TouchableOpacity style={styles._btn} onPress={() => showAlert()}>
+                    <Text style={styles._btn_text}> Update Goals </Text>
+                </TouchableOpacity>
+                <View style={{ paddingBottom: 20 }} />
 
 
-       
-       
-    </View>
-  );
-    
+            </ScrollView>
+
+
+
+
+        </View>
+    );
+
 }
--- health-app/src/pages/ManageGoals/styles.js
+++ health-app/src/pages/ManageGoals/styles.js
@@ -1,147 +1,164 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    _container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        paddingHorizontal: 20,
-      },
-      _back: {
-        // width: 50,
-        // height: 50,
-        // borderRadius: 50 / 2,
-        // alignItems: "center",
-        // justifyContent: "center",
-        marginTop: 40,
-      },
-      _subheading: {
-        color: '#000',
-        fontFamily: 'Comfortaa-Regular',
-        fontSize: 13,
+  _container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+  },
+  _back: {
+    // width: 50,
+    // height: 50,
+    // borderRadius: 50 / 2,
+    // alignItems: "center",
+    // justifyContent: "center",
+    marginTop: 40,
+    marginLeft: 20,
+  },
+  _subheading: {
+    color: '#000',
+    fontFamily: 'Comfortaa-Regular',
+    fontSize: 13,
+    marginLeft: 50,
+  },
+  _leftButton: {
+    width: 85,
+    height: 80,
+  },
+  _title: {
+    color: "#000",
+    fontSize: 15,
+    fontFamily: "Comfortaa-Regular",
+    marginBottom: -50,
+    marginTop: 50,
+    marginLeft: 50,
+  },
+  _btn: {
+    backgroundColor: "#000",
+    height: 50,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    marginTop: 20,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  _btn_text: {
+    color: "#fff",
+    fontSize: 14,
+    fontFamily: "Comfortaa-Regular",
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
+  _heading: {
+    color: "#000",
+    fontSize: 23,
+    fontFamily: "Comfortaa-Regular",
+    marginTop: 10,
+    marginBottom: 20,
+    marginLeft: 10,
+  },
+  _centered_view: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 40,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
     },
-    _leftButton: {
-        width: 85,
-        height: 80,
-    },
-    _title: {
-        color: "#000",
-        fontSize: 15,
-        fontFamily: "Comfortaa-Regular",
-        marginBottom: -50,
-        marginTop: 50,
-    },
-      _btn: {
-        backgroundColor: "#000",
-        height: 50,
-        borderRadius: 5,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "row",
-        marginTop: 20,
-      },
-      _btn_text: {
-        color: "#fff",
-        fontSize: 14,
-        fontFamily: "Comfortaa-Regular",
-        textTransform: "uppercase",
-        fontWeight: "bold",
-      },
-      _heading: {
-        color: "#000",
-        fontSize: 23,
-        fontFamily: "Comfortaa-Regular",
-        marginTop: 10,
-        marginBottom: 20,
-      },
-      _centered_view: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 22
-    },
-    modalView: {
-        margin: 40,
-        backgroundColor: "white",
-        borderRadius: 20,
-        padding: 35,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5
-    },
-    modalText: {
-        textAlign: "center"
-    },_subheading: {
-        color: '#000',
-        fontFamily: 'Comfortaa-Regular',
-        fontSize: 13,
-    },
-    _leftButton: {
-        width: 85,
-        height: 80,
-    },
-    _title: {
-        color: "#000",
-        fontSize: 15,
-        fontFamily: "Comfortaa-Regular",
-        marginBottom: -50,
-        marginTop: 50,
-    },
-      button: {
-        backgroundColor: "#000",
-        height: 50,
-        borderRadius: 5,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "row",
-        marginTop: 20,
-        /*
-        backgroundColor: '#788eec',
-        marginLeft: 30,
-        marginRight: 30,
-        marginTop: 20,
-        height: 48,
-        borderRadius: 5,
-        alignItems: "center",
-        justifyContent: 'center'
-        */
-    },
-    _input_main: {
-      borderWidth: 1.5,
-      borderColor: "#000",
-      marginTop: 20,
-      marginLeft: 80,
-      height: 50,
-      width: 50,
-      paddingHorizontal: 10,
-      borderRadius: 5,
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  modalText: {
+    /*textAlign: "left"*/
+    fontFamily: 'Comfortaa-Regular',
+    fontSize: 15,
+    marginLeft: 40
+  }, _subheading: {
+    color: '#000',
+    fontFamily: 'Comfortaa-Regular',
+    fontSize: 13,
+  },
+  modalText1: {
+    /*textAlign: "left"*/
+    fontFamily: 'Comfortaa-Regular',
+    fontSize: 15,
+    marginLeft: 175,
+    marginTop: -30,
+  },
+  _leftButton: {
+    width: 85,
+    height: 80,
+  },
+  _title: {
+    color: "#000",
+    fontSize: 15,
+    fontFamily: "Comfortaa-Regular",
+    marginBottom: -50,
+    marginTop: 50,
+    marginLeft: 10
+  },
+  button: {
+    backgroundColor: "#000",
+    height: 50,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    marginTop: 20,
+    /*
+    backgroundColor: '#788eec',
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 20,
+    height: 48,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: 'center'
+    */
+  },
+  _input_main: {
+    borderWidth: 1.5,
+    borderColor: "#000",
+    marginTop: 20,
+    marginLeft: 100,
+    height: 50,
+    width: 50,
+    paddingHorizontal: 10,
+    borderRadius: 5,
   },
   _textinput: {
-      color: "#000",
-      fontFamily: "Comfortaa-Regular",
-      fontSize: 16,
-      flex: 1,
+    color: "#000",
+    fontFamily: "Comfortaa-Regular",
+    fontSize: 16,
+    flex: 1,
   },
-    buttonTitle: {
-        color: "#fff",
-        fontSize: 14,
-        fontFamily: "Comfortaa-Regular",
-        textTransform: "uppercase",
-        fontWeight: "bold",
-        /*
-        color: 'white',
-        fontSize: 16,
-        fontWeight: "bold"
-        */
-    },
-      _title_main:{
-        color: "#000",
-        fontSize: 15,
-        fontFamily: "Comfortaa-Regular",
-        marginTop: 10,
-      }
+  buttonTitle: {
+    color: "#fff",
+    fontSize: 14,
+    fontFamily: "Comfortaa-Regular",
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    /*
+    color: 'white',
+    fontSize: 16,
+    fontWeight: "bold"
+    */
+  },
+  _title_main: {
+    color: "#000",
+    fontSize: 15,
+    fontFamily: "Comfortaa-Regular",
+    marginTop: 10,
+  }
 })

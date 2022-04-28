fc4704f9d54c0d3024e972e803355c34aab44af4
samishsK <samish.sk@gmail.com>
2022-04-28 00:54:17 -0700

    Pending tasks button on home page at set time

--- health-app/src/pages/HomeScreen/HomeScreen.js
+++ health-app/src/pages/HomeScreen/HomeScreen.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { Image, Text, View, TouchableOpacity } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import {db, authentication, handleSignOut} from "../../firebase/config";
-import { createUserWithEmailAndPassword,signInWithEmailAndPassword, getAuth} from "firebase/auth";
-import { collection, getDoc,updateDoc,onSnapshot,deleteDoc,doc,setDoc, waitForPendingWrites} from "firebase/firestore";
+import { db, authentication, handleSignOut } from "../../firebase/config";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { collection, getDoc, updateDoc, onSnapshot, deleteDoc, doc, setDoc, waitForPendingWrites } from "firebase/firestore";
 import { createStackNavigator } from '@react-navigation/stack'
 import UpdateProgress from '../updateProgress/UpdateProgress';
 import { useNavigation } from '@react-navigation/native';
@@ -24,67 +24,90 @@ const Stack = createStackNavigator();
 
 export default function HomeScreen(props) {
     //const navigation = useNavigation();
+
+    const showButton = () => {
+        var date = new Date(Date.now());
+        var curr_time = date.getHours();
+        console.log(curr_time);
+        if (curr_time === 0) {
+            return (
+                <TouchableOpacity style={styles.btn}>
+                    <Text style={styles.btn_text} onPress={() => props.navigation.navigate('Pending')} >Remember to Complete Your Pending Tasks! </Text></TouchableOpacity>
+            )
+        }
+    }
+
+
     var [userName, setUserName] = useState("User");
     useEffect(() => {
-        const docRef = doc(db,'users', String(props.user.uid));
+        const docRef = doc(db, 'users', String(props.user.uid));
         getDoc(docRef)
-          .then((doc) => {
-            var docData = doc.data()
-            setUserName(docData["data"]["name"])
-        })
+            .then((doc) => {
+                var docData = doc.data()
+                setUserName(docData["data"]["name"])
+            })
     }, []);
+
     //console.log(props.user);
 
-    
+
     return (
         <View style={styles.container}>
-        <KeyboardAwareScrollView
-            style={{ flex: 1, width: '100%' }}
-            keyboardShouldPersistTaps="always">
 
-            <View
-            style={{ flex: 1, width: '100%', flexDirection: "row", justifyContent: 'space-between' }}>
-                <Text style={styles.title}>Welcome, {userName}!</Text>
+            <KeyboardAwareScrollView
+                style={{ flex: 1, width: '100%' }}
+                keyboardShouldPersistTaps="always">
 
-                <TouchableOpacity
-                    style={styles.profile}
-                    onPress={() => props.navigation.navigate('Profile')}>
+                <View
+                    style={{ flex: 1, width: '100%', flexDirection: "row", justifyContent: 'space-between' }}>
+                    <Text style={styles.title}>Welcome, {userName}!</Text>
 
-                    <Image
+                    <TouchableOpacity
+                        style={styles.profile}
+                        onPress={() => props.navigation.navigate('Profile')}>
+
+                        <Image
                             style={styles.profile}
                             source={require('../../../assets/profile.png')}
-                    />
-                </TouchableOpacity>
-            </View>
+                        />
+                    </TouchableOpacity>
+                </View>
 
-        
-            <TouchableOpacity
+                <View style={{
+                    fontFamily: 'Comfortaa-Regular',
+                    fontSize: 15
+                }}>
+                    {showButton()}
+                </View>
+
+
+                <TouchableOpacity
                     style={styles.button}
                     onPress={() => props.navigation.navigate('Update')}>
                     <Text style={styles.buttonTitle}>Update Progress</Text>
-            </TouchableOpacity>
+                </TouchableOpacity>
 
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => props.navigation.navigate('View')}>
-                <Text style={styles.buttonTitle}>View Progress</Text>
-            </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => props.navigation.navigate('View')}>
+                    <Text style={styles.buttonTitle}>View Progress</Text>
+                </TouchableOpacity>
 
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => props.navigation.navigate('Manage')}
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => props.navigation.navigate('Manage')}
                 >
-                <Text style={styles.buttonTitle}>Manage Goals</Text>
-               
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => props.navigation.navigate('Pending')}
+                    <Text style={styles.buttonTitle}>Manage Goals</Text>
+
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => props.navigation.navigate('Pending')}
                 >
-                <Text style={styles.buttonTitle}>View Pending Tasks</Text>
-               
-            </TouchableOpacity>
-        </KeyboardAwareScrollView>
-    </View>
+                    <Text style={styles.buttonTitle}>View Pending Tasks</Text>
+
+                </TouchableOpacity>
+            </KeyboardAwareScrollView>
+        </View>
     )
 }
